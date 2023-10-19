7e0b37b3a5d5dcddc68352f74d29eee75bc3f3aa
Emil Merle <Emil.Merle@msg.group>
2023-10-19 11:52:38 +0200

    Pushing failed build

--- manager/src/main/java/org/openremote/manager/agent/AgentHealthStatusProvider.java
+++ manager/src/main/java/org/openremote/manager/agent/AgentHealthStatusProvider.java
@@ -32,7 +32,7 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class AgentHealthStatusProvider implements HealthStatusProvider {
 
     public static final String NAME = "agents";
-    protected AgentService agentService;
+    protected AgentService 
 
     @Override
     public int getPriority() {
